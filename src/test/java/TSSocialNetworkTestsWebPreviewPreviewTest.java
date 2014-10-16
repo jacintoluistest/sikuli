43c436b88976fc528e1167b8bdd326a48b5da1e9
jacintoluistest <luisj81@gmail.com>
2014-10-16 18:30:32 -0500

    Update for TestWebPreviewTest

--- src/test/java/TSSocialNetworkTestsWebPreviewPreviewTest.java
+++ src/test/java/TSSocialNetworkTestsWebPreviewPreviewTest.java
@@ -1,32 +1,78 @@
 
 
+import java.io.File;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-//import org.sikuli.basics.SikuliScript;
+import org.sikuli.basics.Settings;
+
 
 
 public class TSSocialNetworkTestsWebPreviewPreviewTest
 {
-	String projectImagesPath = "images//SocialNetworkAnalysis";;
+	//On this path are stored the images used for the project Tests
+		static String projectImagesPath;
 
-	String commonImageMapImagesPath =
-		"images//TomSawyerPerspectives//CommonImageMapPreview//";;
+		//On this path are stores the images used for tool bar on Desktop preview
+		static String webPreviewToolBarImagesPath;
 
-	String imageMapProjectImages = "images//SocialNetworkAnalysis//ImageMap//";;
-
-	static String toolTipsImages =
-		"images//TomSawyerPerspectives//CommonImageMapPreview//HoverToolTips";;
+		//On this path are the tool tips expected for the tests
+		static String webPreviewToolTipsImagePath;
+		
+		//
+		static int currentScreenId=0;
+	
 
 
 	@BeforeClass
 	public static void setUp()
 	{
-		TSAutomationTester = new TSTester();
-		TSAutomationTester.LaunchTS();
-		TSAutomationTester.openProyectWindows(TSAutomationUtils.getProperty("SocialNetworkProyectPath"));
-		TSAutomationTester.launchWebPreview();
+		Settings.MoveMouseDelay = new Float(1.5);
+		System.out.println(TSAutomationUtils.getOs());
+		//Set up Mac Environment
+		if (TSAutomationUtils.getOs().contains("mac os x"))
+
+		{
+			projectImagesPath =
+				"images" + File.separator + "Mac" + File.separator
+					+ "SocialNetworkAnalysis" + File.separator + "ImageMap";
+			webPreviewToolBarImagesPath =
+				"images" + File.separator + "Mac" + File.separator
+					+ "TomSawyerPerspectives" + File.separator + "CommonImageMapPreview"
+					+ File.separator + "ToolBar";
+
+			webPreviewToolTipsImagePath = 	
+				"images" + File.separator + "Mac" + File.separator
+					+ "TomSawyerPerspectives" + File.separator + "CommonImageMapPreview"
+					+ File.separator + "HoverToolTips";
+			TSAutomationTester = new TSTester();
+			TSAutomationTester.LaunchTS();
+			TSAutomationTester.openProjectMac(TSAutomationUtils.getProperty("SocialNetworkProyectPath_Mac"));
+			TSAutomationTester.launchWebPreview();
+		}
+		else
+			//Set up Windows Environment
+		{
+			projectImagesPath =
+				"images" + File.separator + "Windows" + File.separator
+					+ "SocialNetworkAnalysis" + File.separator + "ImageMap";
+
+			webPreviewToolBarImagesPath =
+				"images" + File.separator + "Windows" + File.separator
+					+ "TomSawyerPerspectives" + File.separator + "CommonImageMapPreview"
+					+ File.separator + "ToolBar";
+
+			webPreviewToolTipsImagePath =
+				"images" + File.separator + "Windows" + File.separator
+					+ "TomSawyerPerspectives" + File.separator + "CommonDesktopPreview"
+					+ File.separator + "HoverToolTips";
+			TSAutomationTester = new TSTester();
+			TSAutomationTester.LaunchTS();
+			TSAutomationTester.openProyectWindows(TSAutomationUtils.getProperty("SocialNetworkProyectPath_Win"));
+			TSAutomationTester.launchWebPreview();
+
+		}
 	}
 
 
@@ -35,7 +81,7 @@ public class TSSocialNetworkTestsWebPreviewPreviewTest
 	{
 		System.out.println("testLaunchWebPreview");
 		Assert.assertTrue(TSTestCases.isToolBarPresent(TSAutomationTester,
-			commonImageMapImagesPath + "ToolBar//WebCommonToolBar.png"));
+			projectImagesPath + "ToolBar//WebCommonToolBar.png"));
 
 	}
 
@@ -45,7 +91,7 @@ public class TSSocialNetworkTestsWebPreviewPreviewTest
 	{
 		System.out.println("testIsPresentToolBar");
 		Assert.assertTrue(TSTestCases.isToolBarPresent(TSAutomationTester,
-			imageMapProjectImages + "SecondaryScreen//ToolBarImageMap.png"));
+			projectImagesPath + "SecondaryScreen//ToolBarImageMap.png"));
 	}
 
 
@@ -54,7 +100,7 @@ public class TSSocialNetworkTestsWebPreviewPreviewTest
 	{
 		System.out.println("testIsTableViewPresent");
 		Assert.assertTrue(TSTestCases.isToolBarPresent(TSAutomationTester,
-			imageMapProjectImages + "SecondaryScreen//TableViewImageMap.png"));
+			projectImagesPath + "SecondaryScreen//TableViewImageMap.png"));
 	}
 
 
@@ -65,8 +111,8 @@ public class TSSocialNetworkTestsWebPreviewPreviewTest
 		System.out.println("testHoverSaveAs");
 
 		Assert.assertTrue(TSTestCases.isToolTipPresentImageMap(TSAutomationTester,
-			commonImageMapImagesPath + "Toolbar//SaveAsImageMap.png",
-			commonImageMapImagesPath + "HoverToolTips//SaveAsImage.png"));
+			webPreviewToolBarImagesPath + File.separator +"SaveAsImageMap.png",
+			webPreviewToolTipsImagePath + File.separator +"SaveAsImage.png"));
 	}
 
 
