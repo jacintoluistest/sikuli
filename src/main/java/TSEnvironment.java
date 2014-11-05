c3b99f36e2f1510e7843113d18d74731811b7a36
jacintoluistest <luisj81@gmail.com>
2014-11-05 13:12:03 -0600

    Overload the constructor for create paths for each Browser Supported

--- src/main/java/TSEnvironment.java
+++ src/main/java/TSEnvironment.java
@@ -23,8 +23,39 @@ public class TSEnvironment
 	String testPreviewType;
 	
 	String evidencePath;
+	
+	String browserPath;
 
 
+	public TSEnvironment(String project, int testType, String browser)
+	{
+		
+		
+		switch (testType)
+		{
+			case 1:
+				this.testTypeImagePath = "CommonDesktopPreview";
+				this.testPreviewType="Desktop";
+				break;
+			case 2:
+				this.testTypeImagePath = "CommonImageMapPreview";
+				this.testPreviewType="ImageMap";
+				break;
+			case 3:
+				this.testTypeImagePath = "CommonCanvasPreview";
+				this.testPreviewType="HtmlCanvas";
+				break;
+
+			default:
+				System.out.println("Error");
+
+		}
+		this.browserPath=browser;
+		this.projectName = project;
+		System.out.println("Setting");
+		setEnvironmentWeb();
+	}
+	
 	public TSEnvironment(String project, int testType)
 	{
 		
@@ -52,6 +83,7 @@ public class TSEnvironment
 		System.out.println("Setting");
 		setEnvironment();
 	}
+
 
 
 	public void setEnvironment()
@@ -86,6 +118,46 @@ public class TSEnvironment
 		evidencePath = "images" + File.separator + operativeSystem + File.separator + projectName + File.separator +"Evidence"+File.separator;
 
 	}
+	public void setEnvironmentWeb()
+	{
+		
+		Settings.MoveMouseDelay = new Float(1.5);
+
+		if (TSAutomationUtils.getOs().contains("mac os x"))
+
+			operativeSystem = "Mac";
+		else
+		{
+			operativeSystem = "Windows";
+		}
+
+		projectImagesPath =
+			"images" + File.separator + operativeSystem + File.separator
+				+ this.projectName + File.separator + testPreviewType + File.separator + browserPath;
+		System.out.println(projectImagesPath);
+		toolBarImagesPath =
+			"images" + File.separator + operativeSystem + File.separator
+				+ "TomSawyerPerspectives" + File.separator + testTypeImagePath
+				+ File.separator + browserPath
+				+ File.separator + "ToolBar";
+		System.out.println(toolBarImagesPath);
+
+		toolTipsImagePath =
+			"images" + File.separator + operativeSystem + File.separator
+				+ "TomSawyerPerspectives" + File.separator + testTypeImagePath
+				+File.separator + browserPath
+				+ File.separator + "HoverToolTips";
+		System.out.println(toolTipsImagePath);
+		
+		evidencePath = "images" + File.separator + operativeSystem + File.separator + projectName + File.separator +"Evidence"+File.separator;
+
+	}
+	
+	public void setBrowserPath(String defaultBrowser){
+		
+		
+		
+	}
 
 
 	public final static int DESKTOP_PREVIEW = 1;
@@ -93,5 +165,6 @@ public class TSEnvironment
 	public final static int IMAGEMAP_PREVIEW = 2;
 
 	public final static int HTML5_PREVIEW = 3;
+	
 
 }
