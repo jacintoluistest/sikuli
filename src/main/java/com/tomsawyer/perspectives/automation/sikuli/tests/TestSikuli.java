6c739df723b6eb147d238d567838f272c44d4458
jacintoluistest <luisj81@gmail.com>
2015-03-02 13:23:10 -0600

    conceptual for blank spaces

--- src/main/java/com/tomsawyer/perspectives/automation/sikuli/tests/TestSikuli.java
+++ src/main/java/com/tomsawyer/perspectives/automation/sikuli/tests/TestSikuli.java
@@ -1,5 +1,7 @@
 package com.tomsawyer.perspectives.automation.sikuli.tests;
 
+import org.sikuli.script.Location;
+
 
 
 
@@ -16,10 +18,20 @@ package com.tomsawyer.perspectives.automation.sikuli.tests;
 public class TestSikuli {
 	public static void main(String args[]){
 	
-		org.sikuli.basics.Debug.setDebugLevel(3);
+		//org.sikuli.basics.Debug.setDebugLevel(3);
 		TSAutomationUtils.pauseScript(new Long(3000));
 		TSTester tester = new TSTester(0);
-		tester.fullScreenBrowserMac();
+		Location currentLocation = tester.automationTesterCurrentScreen.getCenter();
+		float xFactor = new Float (0.5);
+		int x = (int)(currentLocation.x +((currentLocation.x * xFactor)));
+		int y = (int)(currentLocation.y +((currentLocation.x * xFactor)));
+		currentLocation.x = x;
+		currentLocation.y = y;
+		currentLocation.doubleClick();
+		currentLocation.click();
+		
+		
+		
 		
 	
 	}
